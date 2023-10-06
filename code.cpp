b99fe141f8ca6f20b9f3fa4571b7f2ea3ce7bb58
tanmaya-2003 <146698807+tanmaya-2003@users.noreply.github.com>
2023-10-06 21:21:15 +0530

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,22 +1,22 @@
 #include<iostream>
+
 using namespace std;
+
 int main(){
-	 int x; // variable name x of type int --> It can store only one value at a time, this is where arrays come into place
-	 
-	 //Arrays can be used to store multiple values in a single variable
-	 int a[5]; // this means it is an array of name a which can store 5 data items of int type
-	   //Note: The indexing of array starts from 0 to 4 for this case
-	   
-	   //elements starting from 0-4
-	   //the first element will have an index 0--> a[0] 
-	   //the second element will have an index 1--> a[1] 
-	   //and goes till a[4]
-	   
-	   int f,b,c,d,e;
-	   cout<<"Give 5 int as input"<<endl;
-	   cin>>f>>b>>c>>d>>e;
-	   
-	   cout<<"reverse order is: ";
-	   cout<<e<<d<<c<<b<<f;
+	//Declare 2 variables and then assign them values of 3,5 and then add them.
+	
+	int a=3;
+	int b=5;
+//	a=3;
+//	b=5;
+	
+//	int c;
+//	c=a+b;
+	
+	cout<<a+b<<endl<<"a+b";
+	
+	cout<<a+b;
+	cout<<endl;
+	cout<<"a+b";
 	return 0;
 }
